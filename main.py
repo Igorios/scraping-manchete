d561a3cdf6272a9ed1277a0f3fd9e8ef8fbec746
Igor Borges <borgesigor153@gmail.com>
2026-01-05 17:32:53 -0300

    :sparkles: feat: ajustando cors da aplicacao

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routers import busca_reposta_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/ping")
 def ping():
