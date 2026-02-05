cf3b00d18d9d38346ad9e1817e715ec5f1e770c5
Igor Borges <borgesigor153@gmail.com>
2026-02-05 16:21:41 -0300

    :recycle: refactor: ajustando nome do valor

--- components/controlador_site.py
+++ components/controlador_site.py
@@ -4,7 +4,7 @@ from services.buscador import *
 def controlador_site(request: BuscarRespostaRequest):
 
     match request.name:
-        case "g1":
+        case "G1 Notícias":
             return buscar_g1(request.urlSite)
         case _:
             return []
